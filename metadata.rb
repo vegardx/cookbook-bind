08084359d34231af7f78323931ee95d8416f7809
Vegard Hansen <vegard.x@gmail.com>
2016-05-29 19:49:27 +0200

    Bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vegard.x@gmail.com'
 license          'GNU Public License 2.0'
 description      'Installs/Configures bind'
 long_description 'Installs/Configures bind'
-version          '0.1.1'
+version          '0.1.2'
 
 supports 'debian'
 depends 'chef-sugar'
